ba173e04809cb1b777ba92144f8a11db75874bd3
Minchione <hoang.nguyen2712@studenti.luiss.it>
2022-07-06 20:09:19 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+    function yesClick() {
+    alert("!");
+  }
+  
+  function noHover()
+  
+  {
+    console.log('Hello');
+    console.log('width', window.innerWidth, window.innerHeight);
+    var x = Math.floor(Math.random() * window.innerWidth);
+    
+    var y = Math.floor(Math.random() * window.innerHeight);
+    console.log('xy', x, y);
+    if (x < 400) {
+        document.getElementById("btnNo").style.left = x + "px";
+    } else {
+        document.getElementById("btnNo").style.left = (x- 400) + "px";
+    }
+    if (y > (window.innerHeight - 150)) {
+        document.getElementById("btnNo").style.top = (y - 600) + "px";
+    } else {
+        document.getElementById("btnNo").style.top = y + "px";
+    }
+    document.getElementById("btnNo").style.position ="absolute";
+  }
